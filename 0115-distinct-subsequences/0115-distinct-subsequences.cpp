6926fd9640bc231704adc81212d06f54f97c502d
Aniket Kumar Pathak <itsaniket2007@gmail.com>
2026-09-06 09:53:00 +0530

    Time: 7 ms (96.2%), Space: 9.1 MB (86.32%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <iostream>
+class Solution {
+public:
+    int numDistinct(std::string s, std::string t) {
+        int m = s.length(), n = t.length();
+        std::vector<unsigned long long> dp(n + 1, 0);
+        dp[0] = 1;
+        for (int i = 1; i <= m; ++i) {
+            for (int j = n; j >= 1; --j) {
+                if (s[i - 1] == t[j - 1]) {
+                    dp[j] += dp[j - 1];
+                }
+            }
+        }
+        return dp[n];
+    }
+};
